b3bff9e10ec4f7bee7fccccbfcdb3567e579f47d
Emi <emi.mano@tum.de>
2022-05-18 14:17:21 +0200

    abgabe

--- src/de/tum/in/ase/eist/ecommerce/ECommerceFacade.java
+++ src/de/tum/in/ase/eist/ecommerce/ECommerceFacade.java
@@ -1,16 +1,50 @@
 package de.tum.in.ase.eist.ecommerce;
 
-public abstract class ECommerceFacade {
-    public abstract void processOrder(Order order);
-    public abstract void processOrder(Order order, String phoneNumber);
-    public abstract Order retreiveLatestOrder(int id);
-    public abstract void playAdvertisement(int ageRestriction);
-    public abstract void shipOrder(Order order, String address);
+public  class ECommerceFacade extends AdvertisementController {
+    private Order processOrder;
+    private Order retrieveLastOrder;
+    private int playAdvertisment;
 
-    public ECommerceFacade(Order order,String phoneNumber,String address,int id, int ageRestriction){
+    public Order getProcessOrder() {
+        return processOrder;
+    }
 
+    public void setProcessOrder(Order processOrder) {
+        this.processOrder = processOrder;
+    }
+
+    public Order getRetrieveLastOrder() {
+        return retrieveLastOrder;
+    }
+
+    public void setRetrieveLastOrder(Order retrieveLastOrder) {
+        this.retrieveLastOrder = retrieveLastOrder;
+    }
+
+    public int getPlayAdvertisment() {
+        return playAdvertisment;
+    }
+
+    public void setPlayAdvertisment(int playAdvertisment) {
+        this.playAdvertisment = playAdvertisment;
     }
 
 
+
+    public ECommerceFacade(){
+
+    }
+    public void processOrder(Order order, String string){}
+    public Order retrieveLatestOrder(Order order){
+        Order order1 = order;
+        return order;
+    }
+
+    @Override
+    public void playAdvertisement(int ageRestriction) {
+        super.playAdvertisement(ageRestriction);
+    }
+
+    public void shipOrder(Order order, String string){}
 
 }
